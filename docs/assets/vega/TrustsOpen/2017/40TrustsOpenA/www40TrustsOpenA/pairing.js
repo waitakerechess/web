36b98a8f904b6b46d4fa623ed7f4b9b09918d532
Bruce Pollard <dopmannz@users.noreply.github.com>
2017-06-03 18:20:03 +1200

    Paired round 3

--- docs/assets/vega/TrustsOpen/2017/40TrustsOpenA/www40TrustsOpenA/pairing.js
+++ docs/assets/vega/TrustsOpen/2017/40TrustsOpenA/www40TrustsOpenA/pairing.js
@@ -1,1 +1,1 @@
-document.write ('<h3>Pairings:</h3><a class="btn btn-default btn-sm" href="pairs1.html"> 1 </a> <a class="btn btn-default btn-sm" href="pairs2.html"> 2 </a> ')
+document.write ('<h3>Pairings:</h3><a class="btn btn-default btn-sm" href="pairs1.html"> 1 </a> <a class="btn btn-default btn-sm" href="pairs2.html"> 2 </a> <a class="btn btn-default btn-sm" href="pairs3.html"> 3 </a> ')
